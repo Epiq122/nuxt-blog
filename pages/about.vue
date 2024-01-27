0c52df8baefbf666748d6688a5fa83ce50af147c
Robert Gleason <l2ob122@gmail.com>
2024-01-26 16:12:10 -0800

    Add navigation bar and create new pages
    This commit adds a navigation bar to the app.vue file, with links to the Home and About pages. It also creates two new pages, about.vue and index.vue, with their respective content.

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>This page is about me</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Hello from main page</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
