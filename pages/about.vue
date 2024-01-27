c378a3d99d390ebccfd224c67e28cd09fac610c7
Robert Gleason <l2ob122@gmail.com>
2024-01-26 16:35:41 -0800

    Showed multiple ways of changing layout

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,21 @@
 <template>
   <div>This page is about me</div>
+  <div>
+    <button @click="enableCustomLayout">Update Layout</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<!-- <script>
+definePageMeta({
+  layout: 'another',
+  title: 'About',
+});
+</script> -->
+
+<script setup>
+function enableCustomLayout() {
+  setPageLayout('another');
+}
+</script>
 
 <style scoped></style>
