c2ebaf7bbc67aa20cb66e28be04e2c7f116c451d
Robert Gleason <l2ob122@gmail.com>
2024-01-26 16:38:33 -0800

    Add @nuxtjs/tailwindcss as a devDependency and enable it in nuxt.config.ts

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,6 @@
 <template>
   <div>This page is about me</div>
-  <div>
-    <button @click="enableCustomLayout">Update Layout</button>
-  </div>
+  <div></div>
 </template>
-
-<!-- <script>
-definePageMeta({
-  layout: 'another',
-  title: 'About',
-});
-</script> -->
-
-<script setup>
-function enableCustomLayout() {
-  setPageLayout('another');
-}
-</script>
 
 <style scoped></style>
