9bb74fcecbf0fe8a5f898f962394d947b2c6e4b4
Robert Gleason <l2ob122@gmail.com>
2024-01-26 19:35:11 -0800

    Showed how to use the fetch

---
+++ components/project-list.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Projects</div>
+  <section :if="pending">Loading....</section>
+  <section :else-if="error">Something went wrong try again......</section>
+  <section :else>Display the repos</section>
+</template>
+
+<script setup>
+const { error, pending, data } = await useFetch(
+  'https://api.github.com/users/epiq122/repos',
+);
+</script>
+
+<style scoped></style>
---
+++ pages/projects.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2 class="text-2xl font-semibold mb-10">Project</h2>
+    <ProjectList />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
